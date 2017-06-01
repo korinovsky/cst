b9ae144fc1dd3b43b41050a26c0c2f54761e165a
Konstantin Korinovsky <k.korinovsky@yandex.ru>
2017-06-01 18:00:40 +0300

    init

---
+++ App.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: kg.korinovskiy
+ * Date: 01.06.2017
+ * Time: 17:12
+ */
+
+defined('BEGIN_TIME') || define('BEGIN_TIME', microtime(true));
+
+class App extends \cst\base\App
+{
+
+}
---
+++ base/App.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: kg.korinovskiy
+ * Date: 01.06.2017
+ * Time: 17:13
+ */
+
+namespace cst\base;
+
+
+class App
+{
+    public static function hello() {
+        echo 'Hello word!';
+    }
+}
